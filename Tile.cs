3455d4e0d1cb7b03371bd6b5a343b7244ffe968b
AlexBailey20 <arb4jr@virginia.edu>
2015-12-10 11:09:03 -0500

    Initial Commit, File Parser
    Set up repo, create File Parser which reads the input file and creates
    Tile objects based on the input

---
+++ Tile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolver
+{
+    class Tile
+    {
+        public char[,] dimensions;
+        public int size;
+        public int rsize;
+        public int csize;
+        public bool solution;
+        public Tile(char[,] input, int row, int col)
+        {
+            dimensions = input;
+            rsize = row;
+            csize = col;
+            size = 0;
+            char c = ' ';
+            solution = false;
+            for(int i=0; i < csize; i++)
+            {
+                for(int j = 0; j < rsize; j++)
+                {
+                    if (dimensions[i, j] != c)
+                    {
+                        size += 1;
+                    }
+                }
+            }
+        }
+    }
+}
